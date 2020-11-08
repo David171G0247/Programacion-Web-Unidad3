35fe5c71a7d3c982ce440a63ea094c8abf90673d
171G0247 <171G0247@rcarbonifera.tecnm.mx>
2020-11-07 21:22:34 -0600

    Actividad 3

---
+++ RazasPerros/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RazasPerros.Models;
+using RazasPerros.Models.ViewModels;
+using RazasPerros.Repositories;
+using System.Linq;
+
+namespace RazasPerros.Controllers
+{
+    public class HomeController : Controller
+    {
+        perrosContext context;
+        public HomeController(perrosContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index(string id)
+        {
+            RazasRepository repos = new RazasRepository();
+            IndexViewModel vm = new IndexViewModel();
+            vm.Razas = id == null ? repos.GetRazas() : repos.GetRazasByLetraInicial(id);
+            vm.LetrasIniciales = repos.GetLetrasIniciales();
+            return View(vm);
+        }
+        [Route("Raza/{id}")]
+        public IActionResult InfoPerros(string id)
+        {
+            RazasRepository repos = new RazasRepository();
+            InfoPerroViewModel vm = new InfoPerroViewModel();
+            vm.Raza = repos.GetRazaByNombre(id);
+            if (vm.Raza == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                vm.OtrasRazas = repos.Get4RandomRazasExcept(id);
+                return View(vm);
+            }
+        }
+        [Route("RazasPorPais")]
+        public IActionResult RazasPorPais()
+        {
+            RazasRepository repos = new RazasRepository();
+            RazasPorPaisViewModel vm = new RazasPorPaisViewModel();
+            var paises = context.Paises.Include(x => x.Razas).OrderBy(x => x.Nombre)
+                .Select(x => new RazasPorPaisViewModel { Pais=x.Nombre, Razas= x.Razas });
+            return View(paises);
+        }
+    }
+}
---
+++ RazasPerros/Repositories/RazasRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RazasPerros.Models;
+using Microsoft.EntityFrameworkCore;
+using RazasPerros.Models.ViewModels;
+
+namespace RazasPerros.Repositories
+{
+    public class RazasRepository
+    {
+        perrosContext context = new perrosContext();
+        public IEnumerable<RazaViewModel> GetRazas()
+        {
+            return context.Razas.OrderBy(x => x.Nombre)
+                .Select(x => new RazaViewModel
+                {
+                    Id = x.Id,
+                    Nombre = x.Nombre
+                });
+        }
+        public IEnumerable<RazaViewModel> GetRazasByLetraInicial(string letra)
+        {
+            return GetRazas().Where(x => x.Nombre.StartsWith(letra));
+        }
+        public IEnumerable<char> GetLetrasIniciales()
+        {
+            return context.Razas.OrderBy(x => x.Nombre).Select(x => x.Nombre.First());
+
+        }
+        public Razas GetRazaByNombre(string nombre)
+        {
+            nombre = nombre.Replace("-", " ");
+            return context.Razas.Include(x => x.Estadisticasraza).
+                Include(x => x.Caracteristicasfisicas).
+                Include(x => x.IdPaisNavigation)
+                .FirstOrDefault(x => x.Nombre == nombre);
+        }
+        public IEnumerable<RazaViewModel> Get4RandomRazasExcept(string nombre)
+        {
+            nombre = nombre.Replace("-", " ");
+            Random r = new Random();
+            return GetRazas().Where(x => x.Nombre != nombre)
+                   .OrderBy(x => r.Next()).Take(4).Select(x => new RazaViewModel { Id = x.Id, Nombre = x.Nombre });
+        }
+    }
+}
